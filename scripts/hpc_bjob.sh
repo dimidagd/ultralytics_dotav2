da8937a74412286c86fe9b2765aa7c07298d03a0
Dimitrios Dagdilelis <dimidagd@gmail.com>
2024-02-17 23:59:00 +0100

    change bsize

--- scripts/hpc_bjob.sh
+++ scripts/hpc_bjob.sh
@@ -66,12 +66,12 @@ if [ "$DDP" = true ] ; then
 fi
 
 
-batch_size=42	
+batch_size=30	
 # Check if numgpus is equal to anything else than NGPU
 if [ "$DDP" = true ] ; then
     echo "numgpus $num"
     # Calculate 128/numgpus and cast to int and divisible by the ngpus variable
-    batch_size=$((45 * num))    
+    batch_size=$((30 * num))    
 fi
 
 # nvidia-smi --query-gpu=index,memory.used,utilization.gpu --format=csv -l 10 &
